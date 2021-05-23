e3aead0f2d52ace303f227d17d7e978858a1b3ef
Lizbeth Baez <lizbaez140799@gmail.com>
2021-05-22 20:38:31 -0700

    cambios liz

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
+{    
     [SerializeField]
     float moveSpeed;
 
     void Update()
     {
-        transform.Translate(Axis * moveSpeed * Time.deltaTime);
+        transform.Translate(Axis.normalized.magnitude * Vector3.forward * moveSpeed * Time.deltaTime);
+        if(Axis != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(Axis.normalized);
+        }
     }
 
     Vector3 Axis
